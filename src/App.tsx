20bc0433bcd74880f699b7a48f7d85f8b7978d2d
Ekansharora26 <aroraekansh839@gmail.com>
2026-03-31 22:13:59 +0530

    Blossom poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MarketMirrorPoc from './Component/MarketMirrorPoc';
 import SeoxPoc from './Component/Seox';
+import BlossomPoc from './Component/Blossom';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MarketMirrorPoc />} />
         <Route path="/seox" element={<SeoxPoc />} />
+        <Route path="/blossom" element={<BlossomPoc />} />
       </Routes>
     </>
   );
